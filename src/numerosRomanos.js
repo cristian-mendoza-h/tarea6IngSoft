dbb5fcfdcbbbfd324839aab183a0a8343e6f3e54
Cristian Mendoza <cristian.mendoza.h1@gmail.com>
2025-02-24 23:29:50 -0400

    feat: se aniadio la logica total para convertir numeros romanos en el numeroRomanos.js y y se agrego la funcionalidad del numero 2 en romanos

--- src/numerosRomanos.js
+++ src/numerosRomanos.js
@@ -1,5 +1,13 @@
 function numeroARomano(num) {
     const valores = [
+        { valor: 100, simbolo: "C" },
+        { valor: 90, simbolo: "XC" },
+        { valor: 50, simbolo: "L" },
+        { valor: 40, simbolo: "XL" },
+        { valor: 10, simbolo: "X" },
+        { valor: 9, simbolo: "IX" },
+        { valor: 5, simbolo: "V" },
+        { valor: 4, simbolo: "IV" },
         { valor: 1, simbolo: "I" }
     ];
 
--- src/numerosRomanos.test.js
+++ src/numerosRomanos.test.js
@@ -4,4 +4,8 @@ describe("Conversión de números a romanos", () => {
     it("convierte 1 en I", () => {
         expect(numeroARomano(1)).toBe("I");
     });
+
+    it("convierte 2 en II", () => {
+        expect(numeroARomano(2)).toBe("II");
+    });
 });
